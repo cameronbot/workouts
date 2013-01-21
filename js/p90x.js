548ed21e40393a3f3f3c5995ffc68bfeeb1f1ea2
ch <cameron.hill@gmail.com>
2013-01-20 20:52:23 -0800

    Pushing to GitHub

---
+++ js/p90x.js
@@ -0,0 +1,361 @@
+var p90xTemplate = {
+	"program": {
+		"name": "P90X",
+		"workouts": [
+			{
+				"name": "Chest & Back",
+				"exercises": [
+					{ "name": "Standard Push-ups 1", "options": ["reps"] },
+					{ "name": "Wide Front Pull-ups 1", "options": ["reps"] },
+					{ "name": "Military Push-ups 1", "options": ["reps"] },
+					{ "name": "Reverse Grip Chin-ups 1", "options": ["reps"] },
+					{ "name": "Wide Fly Push-ups 1", "options": ["reps"] },
+					{ "name": "Closed Grip Overhand Pull-ups 1", "options": ["reps"] },
+					{ "name": "Decline Push-ups 1", "options": ["reps"] },
+					{ "name": "Heavy Pants 1", "options": ["reps", "weight"] },
+					{ "name": "Diamond Push-ups 1", "options": ["reps"] },
+					{ "name": "Lawnmowers 1", "options": ["reps", "weight"] },
+					{ "name": "Dive Bomber Push-ups 1", "options": ["reps"] },
+					{ "name": "Back Flys 1", "options": ["reps", "weight"] },
+					{ "name": "Wide Front Pull-ups 2", "options": ["reps"] },
+					{ "name": "Standard Push-ups 2", "options": ["reps"] },
+					{ "name": "Reverse Grip Chin-ups 2", "options": ["reps"] },
+					{ "name": "Military Push-ups 2", "options": ["reps"] },
+					{ "name": "Closed Grip Overhand Pull-ups 2", "options": ["reps"] },
+					{ "name": "Wide Fly Push-ups 2", "options": ["reps"] },
+					{ "name": "Heavy Pants 2", "options": ["reps", "weight"] },
+					{ "name": "Decline Push-ups 2", "options": ["reps"] },
+					{ "name": "Lawnmowers 2", "options": ["reps", "weight"] },
+					{ "name": "Diamond Push-ups 2", "options": ["reps"] },
+					{ "name": "Back Flys 2", "options": ["reps", "weight"] },
+					{ "name": "Dive-bomber Push-ups 2", "options": ["reps"] }
+				]
+			},
+			{
+				"name": "Shoulders & Arms",
+				"exercises": [
+					{ "name": "Alternating Shoulder Presses", "options": ["reps", "weight"] },
+					{ "name": "In & Out Bicep Curls", "options": ["reps", "weight"] },
+					{ "name": "Two Arm Tricep Kickback", "options": ["reps", "weight"] },
+					{ "name": "Alternating Shoulder Presses", "options": ["reps", "weight"] },
+					{ "name": "In & Out Bicep Curls", "options": ["reps", "weight"] },
+					{ "name": "Two Arm Tricep Kickback", "options": ["reps", "weight"] },
+					{ "name": "Deep Swimmer's Presses", "options": ["reps", "weight"] },
+					{ "name": "Full Supination Concentration Curls", "options": ["reps", "weight"] },
+					{ "name": "Chair Dips", "options": ["reps"] },
+					{ "name": "Deep Swimmer's Presses", "options": ["reps", "weight"] },
+					{ "name": "Full Supination Concentration Curls", "options": ["reps", "weight"] },
+					{ "name": "Chair Dips", "options": ["reps"] },
+					{ "name": "Upright Rows", "options": ["reps", "weight"] },
+					{ "name": "Static Arm Curls", "options": ["reps", "weight"] },
+					{ "name": "Flip-Grip Twist Tricep Kickbacks", "options": ["reps", "weight"] },
+					{ "name": "Upright Rows", "options": ["reps", "weight"] },
+					{ "name": "Static Arm Curls", "options": ["reps", "weight"] },
+					{ "name": "Flip-Grip Twist Tricep Kickbacks", "options": ["reps", "weight"] },
+					{ "name": "Seated Two-angle Shoulder Flys", "options": ["reps", "weight"] },
+					{ "name": "Crouching Cohen Curls", "options": ["reps", "weight"] },
+					{ "name": "Lying Down Tricep Extensions", "options": ["reps", "weight"] },
+					{ "name": "In & Out Straight Arm Shoulder Flys", "options": ["reps", "weight"] },
+					{ "name": "Congdon Curls", "options": ["reps", "weight"] },
+					{ "name": "Side Tri-Rises", "options": ["reps-left", "reps-right"] },
+					{ "name": "In & Out Straight-Arm Shoulder Flys", "options": ["reps", "weight"] },
+					{ "name": "Congdon Curls", "options": ["reps", "weight"] },
+					{ "name": "Side Tri-Rises", "options": ["reps-left", "reps-right"] }
+				]
+			},
+			{
+				"name": "Legs & Back",
+				"exercises": [
+					{ "name": "Balanced Lunges", "options": ["reps-left", "reps-right" ] },
+					{ "name": "Calf-raise Squats", "options": ["reps", "weight"] },
+					{ "name": "Reverse Grip Chin-ups", "options": ["reps"] },
+					{ "name": "Super Skaters", "options": ["reps-right", "reps-left"] },
+					{ "name": "Wall Squats", "options": ["time"] },
+					{ "name": "Wide Front Pull-ups", "options": ["reps"] },
+					{ "name": "Step Back Lunge", "options": ["reps", "weight"] },
+					{ "name": "Closed Grip Overhead Pull-ups", "options": ["reps"] },
+					{ "name": "Single Leg Wall Squat", "options": ["time"] },
+					{ "name": "Deadlift Squats", "options": ["reps-left", "reps-right"] },
+					{ "name": "Switch Grip Pull-ups", "options": ["reps"] },
+					{ "name": "Three Way Lunge", "options": ["reps-left", "reps-right"] },
+					{ "name": "Sneaky Lunge", "options": ["reps"] },
+					{ "name": "Reverse Grip Chin-ups", "options": ["reps"] },
+					{ "name": "Chair Salutations", "options": ["time"] },
+					{ "name": "Toe-roll Iso Lunge", "options": ["reps", "weight"] },
+					{ "name": "Wide Front Pull-ups", "options": ["reps"] },
+					{ "name": "Groucho Walk", "options": ["time"] },
+					{ "name": "Calf Raises", "options": ["reps", "weight"], "todo": "There are three versions of these that could be recorded separately." },
+					{ "name": "Closed Grip Overhand Pull-ups", "options": ["reps"] },
+					{ "name": "80/20 Siebers-speed Squats", "options": ["reps-right", "reps-left"] },
+					{ "name": "Switch Grip Pull-ups", "options": ["reps"] }
+				]
+			},
+			{
+				"name": "Chest, Shoulders & Triceps",
+				"exercises": [
+					{ "name": "Slo-mo 3-in-1 Push-ups", "options": ["reps"] },
+					{ "name": "In & Out Sholder Flys", "options": ["reps", "weight"] },
+					{ "name": "Chair Dips", "options": ["reps"] },
+					{ "name": "Plange Push-ups", "options": ["reps"] },
+					{ "name": "Pike Presses", "options": ["reps"] },
+					{ "name": "Side Tri-rises", "options": ["reps-left", "reps-right"] },
+					{ "name": "Floor Flys", "options": ["reps"] },
+					{ "name": "Scarecrows", "options": ["reps", "weight"] },
+					{ "name": "Overhead Tricep Extensions", "options": ["reps", "weight"] },
+					{ "name": "Two-twitch Speed Push-ups", "options": ["reps"] },
+					{ "name": "Y-Presses", "options": ["reps", "weight"] },
+					{ "name": "Lying Tricep Extensions", "options": ["reps", "weight"] },
+					{ "name": "Side-to-side Push-ups", "options": ["reps"] },
+					{ "name": "Pour Flys", "options": ["reps", "weight"] },
+					{ "name": "Side Leaning Tricep Extensions", "options": ["reps", "weight"] },
+					{ "name": "One-arm Push-ups", "options": ["reps"] },
+					{ "name": "Weighted Circles", "options": ["reps", "weight"] },
+					{ "name": "Throw the Bomb", "options": ["reps", "weight"] },
+					{ "name": "Clap or Plyo Push-ups", "options": ["reps"] },
+					{ "name": "Slo-mo Throws", "options": ["reps", "weight"] },
+					{ "name": "One-arm Balance Push-ups", "options": ["reps"] },
+					{ "name": "Fly-Row-Presses", "options": ["reps", "weight"] },
+					{ "name": "Dumbbell Cross-body Blows", "options": ["reps", "weight"] }
+				]
+			},
+			{
+				"name": "Back & Biceps",
+				"exercises": [
+					{ "name": "Wide Front Pull-ups", "options": ["reps"] },
+					{ "name": "Lawnmowers", "options": ["reps", "weight"] },
+					{ "name": "Twenty-ones", "options": ["reps", "weight"] },
+					{ "name": "One-arm Cross-body Curls", "options": ["reps", "weight"] },
+					{ "name": "Switch Grip Pull-ups", "options": ["reps"] },
+					{ "name": "Elbows-out Lawnmowers", "options": ["reps", "weight"] },
+					{ "name": "Standing Bicep Curls", "options": ["reps", "weight"] },
+					{ "name": "One-arm Concentration Curls", "options": ["reps", "weight"] },
+					{ "name": "Corn Cob Pull-ups", "options": ["reps"] },
+					{ "name": "Reverse Grip Bent-over Rows", "options": ["reps", "weight"] },
+					{ "name": "Open-arm Curls", "options": ["reps", "weight"] },
+					{ "name": "Static-arm Curls", "options": ["reps", "weight"] },
+					{ "name": "Towel Pull-ups", "options": ["reps"] },
+					{ "name": "Congdon Locomotives", "options": ["reps", "weight"] },
+					{ "name": "Crouching Cohen Curls", "options": ["reps", "weight"] },
+					{ "name": "One-arm Corkscrew Curls", "options": ["reps", "weight"] },
+					{ "name": "Chin-ups", "options": ["reps"] },
+					{ "name": "Seated Bent-over Back Flys", "options": ["reps", "weight"] },
+					{ "name": "Curl-up/Hamer-downs", "options": ["reps", "weight"] },
+					{ "name": "Hammer Curls", "options": ["reps", "weight"] },
+					{ "name": "Max Rep Pull-ups", "options": ["reps"] },
+					{ "name": "Superman", "options": ["reps"] },
+					{ "name": "In-Out Hammer Curls", "options": ["reps", "weight"] },
+					{ "name": "Strip Set Curls", "options": ["reps", "weight"], "todo": "There are 4 rounds of these that could be tracked separately." }
+				]
+			}
+		]
+	}
+};
+
+/*
+	init the working program object which is basically a clone of the template
+	object and some meta data + functions from the prototype
+*/
+function Program (template, storeName) {
+
+	var savedProgram = JSON.parse(localStorage.getItem(storeName));
+
+	if(savedProgram) {
+		this.program = savedProgram;
+	} else {
+		this.program = template.program;
+		this.program.metadata = { "activeWorkout": 0, "storeName": storeName };
+	}
+	this.logger = this.logger();
+}
+
+Program.prototype = {
+	toString: function() {
+		var storeName = this.program.metadata.storeName,
+			savedProgram = this.readState();
+		console.log(storeName, savedProgram || "Nothing saved yet");
+	},
+	editLog: function (workoutIdx, exerciseIdx, data, index) {
+		console.log(arguments);
+		var curExercise = this.program.workouts[workoutIdx].exercises[exerciseIdx];
+
+		if(!curExercise.datalog) {
+			curExercise.datalog = [];
+		}
+
+		if(index) {
+			curExercise.datalog[index] = data;
+		} else {
+			curExercise.datalog.push(data);
+		}
+
+		console.log("datalog", curExercise.datalog);
+
+		return curExercise.datalog;
+	},
+	log: function (data) {
+		var md = this.program.metadata,
+			wk = this.program.workouts[md.activeWorkout];
+		return this.editLog( md.activeWorkout, wk.activeExercise, data );
+	},
+	logger: function() {
+		var _this = this,
+			logger = function(e) {
+
+				var $elem = $(this).closest('form'),
+					$inputs = $elem.find('input'),
+					data = {},
+					datalog;
+
+				data.date = new Date().toString();
+				data.data = {};
+
+				$inputs.each(function(k) {
+					var $i = $(this);
+					data.data[$i.attr('name')] = $i.val();
+				});
+
+				console.log('data to be saved:', data);
+				datalog = _this.log(data);
+				
+				$elem.hide().siblings('.alert-success').show();
+				_this.refreshHistory($elem.parent(), datalog);
+				_this.next();
+				_this.saveState();
+				
+				setTimeout(function() {
+					_this.updateProgress($('#nav .bar'));
+					_this.swipe.next();
+				}, 100);
+				
+			};
+		return logger;
+	},
+	next: function() {
+		var md = this.program.metadata,
+			wk = this.program.workouts[md.activeWorkout];
+
+		if ( wk.activeExercise === wk.exercises.length ) {
+			// done with this workout!
+			md.activeWorkout = ( md.activeWorkout + 1 ) % md.workouts.length;
+			wk.activeExercise = 0;
+
+			console.log('all done! congrats!');
+		} else {
+			wk.activeExercise += 1;
+
+			console.log('activeExercise:', wk.activeExercise);
+		}
+	},
+	saveState: function() {
+		localStorage.setItem(this.program.metadata.storeName, JSON.stringify(this.program));
+	},
+	readState: function() {
+		return JSON.parse(localStorage.getItem(this.program.metadata.storeName));
+	},
+	clear: function() {
+		localStorage.removeItem(this.program.metadata.storeName);
+	},
+	refreshHistory: function(elem, datalog) {
+		var $elem = $(elem),
+			$history = $(elem).find('.history').html(''),
+			$row,
+			len = datalog.length,
+			text = "";
+
+		if(!$history.length) {
+			$history = $('<table class="history table table-striped"><thead><tr><th>#</th><th>Date</th><th>Log</th><tbody></tbody></table>'),
+			$elem.append($history);
+
+			$history = $history.find('tbody');
+		}
+
+		$.each(datalog, function(k, v) {
+			$row = $('<tr></tr>');
+
+			$row.append('<td>' + (len-k) + '</td>');
+			$row.append('<td>' + v.date + '</td>');
+
+			text = "";
+			$.each(v.data, function(k, v) {
+				text += k.charAt(0).toUpperCase() + k.slice(1) + ": " + v + "<br/>";
+			});
+
+			$row.append('<td>' + text + '</td>');
+
+			$history.append($row);
+		});
+		
+		$history.html($history.find('tr').get().reverse());
+	},
+	updateProgress: function(elem) {
+		var $elem = $(elem),
+			md = this.program.metadata,
+			wk = this.program.workouts[md.activeWorkout],
+			width = Math.ceil(wk.activeExercise / wk.exercises.length * 100);
+
+		console.log('progress:', width);
+		$elem.width(width + '%');
+	},
+	loadWorkout: function(el, index, exercise) {
+		var $temp,
+			$list = $('<ul></ul>'),
+			wk = this.program.workouts[index],
+			_this = this;
+
+		this.program.metadata.activeWorkout = index;
+		if(!exercise) {
+			wk.activeExercise = 0;
+		}
+		
+		$.each(wk.exercises, function(k, v) {
+			$temp = $('<li class="exercise"></li>');
+			$temp.append("<h3>" + v.name + "</h3>");
+
+			var $form = $('<form class="form-horizontal"></form>');
+
+			$.each(v.options, function(k, v) {
+				var prettyName = v.charAt(0).toUpperCase() + v.slice(1);
+				$form.append('<div class="control-group"><label class="control-label" for="' + v + '">' + prettyName + '</label><div class="controls"><input type="text" class="input-medium" name="' + v + '" placeholder="' + prettyName + '"/></div></div>');
+			});
+
+			$form.append('<div class="control-group"><div class="controls"><span class="save btn btn-primary">Save</span><span class="skip btn btn-link pull-right">Skip</span></div></div>');
+			$temp.append($form);
+
+			$temp.append('<div class="alert alert-success">Good Job!</div>');
+			console.log(v);
+			if(v.datalog) {
+				_this.refreshHistory($temp, v.datalog);
+			}
+			$list.append($temp);
+		});
+
+		$(el).append($list);
+		$('#menu').hide();
+		$('#nav').show();
+		$('#nav-bottom').show();
+		
+		this.swipe = new Swipe(document.getElementById('slider'), { "startSlide": wk.activeExercise });
+		this.updateProgress($('#nav .bar'));
+
+	}
+};
+
+/*
+
+	on slides < activeExercise (when navigating backwards), save button becomes Edit button which will update
+	the last entry in the datalog
+
+	add a Stop Workout button
+		- You're not done yet! you can save your progress and resume later or throw this session data away.
+			Save for later? Yes. No.
+		- if yes:
+			add metadata.resume = true;
+			(on load, check if metadata.resume == true,
+				then ask if you wish to Resume from where you left off or start over?
+					if so, skip to activeExercise slide,
+					else, for all uncompleted exercises (exercises > activeExercise), log {skip},
+						then reset activeExercise to 0 and show workout list.
+	
+	add a Skip button
+		- log an empty data point
+*/
